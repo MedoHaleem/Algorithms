2cb093bd3597643de4c42a9ceedfdbefcc221d85
Medo Haleem <medo_e3@hotmail.com>
2019-03-22 01:15:08 +0200

    working on remove in binary tree

--- BinaryTree.js
+++ BinaryTree.js
@@ -35,29 +35,68 @@ class BinarySearchTree {
         }
     }
 
-    lookup(value) {
+    lookup(value){
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        while(currentNode){
+            if(value < currentNode.value){
+                currentNode = currentNode.left;
+            } else if(value > currentNode.value){
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+        return null
+    }
+
+    remove(value) {
         if (!this.root) {
             return null;
         } else {
             let currentNode = this.root;
-            while (currentNode) {
-                if (value < currentNode.value) {
+            let parentNode = null;
+            while (true) {
+                if (!currentNode.left && !currentNode.left) {
+                    return currentNode = null;
+                } else if (value < currentNode.value) {
+                    parentNode = currentNode;
                     currentNode = currentNode.left;
-                }
-                if (value > currentNode.value) {
+                } else if (value > currentNode.value) {
+                    parentNode = currentNode;
                     currentNode = currentNode.right;
-                }
-                if (value === currentNode.value) {
-                    return true
-                }
-                if (!currentNode.left && !currentNode.right) {
-                    return false
+                } else if (value === currentNode.value) {
+                    // No right Child
+                   if (!currentNode.right) {
+                     if (!parentNode) {
+                       this.root = currentNode.left
+                     } else {
+                         if (currentNode.value < parentNode.value) {
+                             return parentNode.left = currentNode.left
+                         } else if(currentNode.value > parentNode.value) {
+                             return  parentNode.right = currentNode.left;
+                         }
+                     }
+                   }
+                   // Right child doesn't have a left child
+                   else if(!currentNode.right.left) {
+                       currentNode.right.left = currentNode.left;
+                       if(parentNode === null) {
+                           this.root = currentNode.right;
+                       } else {
+                           if(currentNode.value < parentNode.value) {
+                               return parentNode.left = currentNode.right;
+                           } else if (currentNode.value > parentNode.value) {
+                               return  parentNode.right = currentNode.right;
+                           }
+                       }
+                   }
                 }
             }
         }
     }
-
-    // remove
 }
 
 const tree = new BinarySearchTree();
@@ -69,7 +108,10 @@ tree.insert(170);
 tree.insert(15);
 tree.insert(1);
 console.log(JSON.stringify(traverse(tree.root)));
-console.log(tree.lookup(20));
+console.log(tree.lookup(4));
+tree.remove(4);
+console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree.lookup(4));
 
 //     9
 //  4     20
